2ff8613d216c1d357acbf925928ee742f16077bf
Web Development2 <Web Development2@ru.local>
2024-09-16 18:22:42 +0530

    new chnages fronendsssssssssssss

--- tinytiaraa/client/src/MainSection/Sectionslider/Slidertext.jsx
+++ tinytiaraa/client/src/MainSection/Sectionslider/Slidertext.jsx
@@ -24,25 +24,25 @@ function Slidertext() {
 
                     <h1>Kid-Safe and Playful</h1>
                     <p>Our Proudly Designed Children's Jewelry Collection</p>
-                    <button onClick={()=>{navigate("/products")}}>Check-it-Out</button>
+                    <button onClick={()=>{navigate("/products")}}>Check-It-Out</button>
                 </div>
             </div>
             <div className="slidertextright">
                 <Slider {...settings}>
                     <div className='sliderrightimg'>
-                        <img src="https://res.cloudinary.com/ddaef5aw1/image/upload/v1724654413/products/jwkffiul1iuwvxuyskcy.png" alt="" />
+                        <img src="https://res.cloudinary.com/ddaef5aw1/image/upload/v1725535147/products/mitharfwyizojclawhpk.png" alt="" />
                     </div>
                     <div className='sliderrightimg'>
-                        <img src="https://lirp.cdn-website.com/48f148a6/dms3rep/multi/opt/TPN+1143+3D+Y-1495w.png" alt="" />
+                        <img src="https://res.cloudinary.com/ddaef5aw1/image/upload/v1724410194/products/yz0szvwhccnhl1fhrbtc.png" alt="" />
                     </div>
                     <div className='sliderrightimg'>
-                        <img src="https://lirp.cdn-website.com/48f148a6/dms3rep/multi/opt/TPN-1107-B+3D+W+B-1495w.png" alt="" />
+                        <img src="https://res.cloudinary.com/ddaef5aw1/image/upload/v1725106811/products/tir9k74t1qr2s8ht8otx.png" alt="" />
                     </div>
                     <div className='sliderrightimg'>
-                        <img src="https://lirp.cdn-website.com/48f148a6/dms3rep/multi/opt/TPN-1118+m+3D+R+S-1495w.png" alt="" />
+                        <img src="https://res.cloudinary.com/ddaef5aw1/image/upload/v1724481112/products/ed4ipy6itiaselgwf9jg.png" alt="" />
                     </div>
                     <div className='sliderrightimg'>
-                        <img src="https://d2j6dbq0eux0bg.cloudfront.net/images/90976509/4345281404.png" alt="" />
+                        <img src="https://res.cloudinary.com/ddaef5aw1/image/upload/v1725002248/products/dvs1rd5zhsabkjcfdfmd.png" alt="" />
                     </div>
 
                 </Slider>
