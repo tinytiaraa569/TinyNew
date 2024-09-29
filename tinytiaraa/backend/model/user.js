5817c3fad38672319655250287b1a1e358634735
tinytiaraa569 <tinytiaraa569@gmail.com>
2024-09-30 01:20:01 +0530

    Update user.js

--- tinytiaraa/backend/model/user.js
+++ tinytiaraa/backend/model/user.js
@@ -49,11 +49,11 @@ const userSchema = new mongoose.Schema({
   avatar: {
     public_id: {
       type: String,
-      required: true,
+      // required: true,
     },
     url: {
       type: String,
-      required: true,
+      // required: true,
     },
   },
   referralBalance: {
