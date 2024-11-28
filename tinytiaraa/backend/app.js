6684489b43d2d976c6944aeeeaf14f0beb8f27b8
Web Development2 <Web Development2@ru.local>
2024-11-28 13:16:16 +0530

    new integration of visitors datass

--- tinytiaraa/backend/app.js
+++ tinytiaraa/backend/app.js
@@ -429,19 +429,19 @@ app.get('/api/conversion-rates', (req, res) => {
 
 // updated code from here 
 // The service account credentials 
-// const credentials = {
-//     "type": "service_account",
-//     "project_id": process.env.GOOGLE_CLOUD_PROJECT_ID,
-//     "private_key_id": process.env.GOOGLE_CLOUD_PRIVATE_KEY_ID,
-//     "private_key": process.env.GOOGLE_CLOUD_PRIVATE_KEY.replace(/\\n/g, '\n'),
-//     "client_email": process.env.GOOGLE_CLOUD_CLIENT_EMAIL,
-//     "client_id": process.env.GOOGLE_CLOUD_CLIENT_ID,
-//     "auth_uri": process.env.GOOGLE_auth_uri,
-//     "token_uri": process.env.GOOGLE_token_uri,
-//     "auth_provider_x509_cert_url": process.env.GOOGLE_auth_provider_x509_cert_url,
-//     "client_x509_cert_url": process.env.GOOGLE_CLOUD_CLIENT_X509_CERT_URL,
-//     "universe_domain": process.env.GOOGLE_universe_domain
-//   }
+const credentials = {
+    "type": "service_account",
+    "project_id": process.env.GOOGLE_CLOUD_PROJECT_ID,
+    "private_key_id": process.env.GOOGLE_CLOUD_PRIVATE_KEY_ID,
+    "private_key": process.env.GOOGLE_CLOUD_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    "client_email": process.env.GOOGLE_CLOUD_CLIENT_EMAIL,
+    "client_id": process.env.GOOGLE_CLOUD_CLIENT_ID,
+    "auth_uri": process.env.GOOGLE_auth_uri,
+    "token_uri": process.env.GOOGLE_token_uri,
+    "auth_provider_x509_cert_url": process.env.GOOGLE_auth_provider_x509_cert_url,
+    "client_x509_cert_url": process.env.GOOGLE_CLOUD_CLIENT_X509_CERT_URL,
+    "universe_domain": process.env.GOOGLE_universe_domain
+  }
   // Authenticate Google Analytics Data APIs
   const auth = new google.auth.GoogleAuth({
     credentials,
